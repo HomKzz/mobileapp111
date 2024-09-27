b113c19d53e2f567ecae14efc359d7d72841d7ca
HomKzz <macrotors2546@gmail.com>
2024-09-27 08:52:05 +0700

    Update App.js

--- App.js
+++ App.js
@@ -110,7 +110,7 @@ export default function App() {
             options={{ headerShown: false }}
           />
           <Tab.Screen 
-            name="ตั้งค่า" 
+            name="ตั้งค่า " 
             component={SettingsStack} 
             options={{ headerShown: false }}
           />
