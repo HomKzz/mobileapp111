8e035eb6bb9d965fb3b050cf0365e8171e6d1c4c
HomKzz <macrotors2546@gmail.com>
2024-09-27 09:52:57 +0700

    Revert "1"
    This reverts commit 6dba17ecc2f670fc222cbd17c1c102acc1a2cf66.

--- App.js
+++ App.js
@@ -110,7 +110,7 @@ export default function App() {
             options={{ headerShown: false }}
           />
           <Tab.Screen 
-            name="ตั้งค่า"
+            name="ตั้งค่า" ///tetstststststststztatatatastst
             component={SettingsStack} 
             options={{ headerShown: false }}
           />
